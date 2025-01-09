d6d846d70bb88b6b8ce5ad3aa16c12e4ecdc05e9
nltsravi <ravi@neolumina.in>
2025-01-09 18:48:30 +0530

    routing added new

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,20 +8,22 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/ntsweb">
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
+         
           <Routes>
-            <Route path="/ntsweb/" element={<Home />} />
-            <Route path="/ntsweb/about" element={<About />} />
-            <Route path="/ntsweb/careers" element={<Careers />} /> 
-            <Route path="/ntsweb/contact" element={<Contact />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/careers" element={<Careers />} /> 
+            <Route path="/contact" element={<Contact />} />
           </Routes>
+          
         </main>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from "path";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/ntsweb/",
+  plugins: [react(), ghPages()],
+  base: "/ntsweb",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
